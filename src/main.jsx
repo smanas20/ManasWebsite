1894343f19ad489bc8f5adcc32e1dd6b7f3c40e9
shuklam20 <manasshukla2011@gmail.com>
2025-04-10 16:55:33 -0400

    Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+// Entry file for Vite or Create React App
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PortfolioWebsite from './PortfolioWebsite';
+
+ReactDOM.render(<PortfolioWebsite />, document.getElementById('root'));
